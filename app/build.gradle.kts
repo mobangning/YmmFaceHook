78dd28701c8c1df5a1a69b38761a82d8026e3a3c
mobangning <143095748+mobangning@users.noreply.github.com>
2026-04-28 23:49:01 +0800

    添加 app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.ymm.facehook"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.ymm.facehook"
+        minSdk = 27
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    compileOnly("de.robv.android.xposed:api:82")
+}
